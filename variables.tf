aa57150a08c2c9ba25ae78fcf23673817ac9b6b3
VARSHA SAHU <65167940+Varsha-devops@users.noreply.github.com>
2026-03-29 20:04:53 +0530

    Comment out ami_id variable in variables.tf
    Comment out the ami_id variable definition in variables.tf

--- variables.tf
+++ variables.tf
@@ -9,10 +9,10 @@ variable "developer_name" {
   type        = string
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-}
+#variable "ami_id" {
+ # description = "AMI ID for EC2 instance"
+  #type        = string
+#}
 
 variable "instance_type" {
   description = "EC2 instance type"
