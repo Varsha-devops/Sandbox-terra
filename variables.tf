856328f4ef1e65052467cf2490fa229870c3c393
VARSHA SAHU <65167940+Varsha-devops@users.noreply.github.com>
2026-03-29 21:04:37 +0530

    Refactor variables in variables.tf for clarity
    Updated variable descriptions and defaults in variables.tf. Removed commented-out variables for clarity.

--- variables.tf
+++ variables.tf
@@ -1,48 +1,20 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
 variable "developer_name" {
-  description = "Developer name who owns the sandbox EC2"
+  description = "Developer name"
   type        = string
 }
-
-#variable "ami_id" {
- # description = "AMI ID for EC2 instance"
-  #type        = string
-#}
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-
-  validation {
-    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
-    error_message = "Only approved instance types are allowed: t3.micro, t3.small, t3.medium."
-  }
-}
-
-variable "subnet_id" {
-  description = "Subnet ID where EC2 will be launched"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
+  default = "t3.micro"
 }
 
 variable "key_name" {
-  description = "Optional EC2 key pair name"
-  type        = string
-  default     = null
+  default = null
 }
 
 variable "allowed_ingress_cidr" {
-  description = "CIDR block allowed for SSH access"
-  type        = string
-  default     = "0.0.0.0/0"
+  default = "0.0.0.0/0"
 }
